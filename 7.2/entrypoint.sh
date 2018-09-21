556574f5c3c3374bd5e30800af88a1bdd14f478b
Louis PIRES--AGIER <louis@pockost.com>
2018-09-21 12:10:07 +0200

    :whale: add Dockerfile and entrypoint to expose dep command

---
+++ 7.2/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $DEPLOYER_VERSION != "latest" ]; then
+
+  url="https://deployer.org/releases/v$DEPLOYER_VERSION/deployer.phar"
+
+else
+
+  url="https://deployer.org/deployer.phar"
+
+fi
+
+status=$(curl -LO -s -w %{http_code} $url)
+
+if [ $status == 200 ]; then
+
+  mv deployer.phar /usr/local/bin/dep
+
+  chmod +x /usr/local/bin/dep
+
+  dep
+
+else
+
+  rm deployer.phar
+  echo "You specify an unavailable version of Deployer. Please look at https://deployer.org/download to choose a correct version."
+  exit
+
+fi
