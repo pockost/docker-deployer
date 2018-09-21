15644eb5a09a1aab9909d12fe9d6a88d8b61438a
Louis PIRES--AGIER <louis@pockost.com>
2018-09-21 16:08:38 +0200

    :zap: improve entrypoint

--- 7.2/entrypoint.sh
+++ 7.2/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $DEPLOYER_VERSION != "latest" ]; then
+if [ -z $DEPLOYER_VERSION ]; then
 
-  url="https://deployer.org/releases/v$DEPLOYER_VERSION/deployer.phar"
+  url="https://deployer.org/deployer.phar"
 
 else
 
-  url="https://deployer.org/deployer.phar"
+  url="https://deployer.org/releases/v$DEPLOYER_VERSION/deployer.phar"
 
 fi
 
@@ -15,13 +15,13 @@ status=$(curl -LO -s -w %{http_code} $url)
 if [ $status == 200 ]; then
 
   mv deployer.phar /usr/local/bin/dep
-
   chmod +x /usr/local/bin/dep
 
 else
 
   rm deployer.phar
-  echo "You specify an unavailable version of Deployer. Please look at https://deployer.org/download to choose a correct version."
+  echo "You specify an unavailable version of Deployer."
+  echo "Please look at https://deployer.org/download to choose a correct version."
   exit
 
 fi
